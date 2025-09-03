abfc9ad537d9c6652673131b4b1c197fbc06bf71
priyanka-pawar044 <pawarpriyanka6673@gmail.com>
2025-09-03 15:25:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.92.0"
+      version = "~> 5.0"
     }
   }
 }
@@ -11,27 +11,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "myenv" {
-  type    = string
-  default = "test"
-}
-
-variable "env" {
-  type = map(any)
-  default = {
-    prod = "t2.small"
-    test = "t2.micro"
-    dev  = "t2.medium"
-  }
-}
-
+# EC2 Instance
 resource "aws_instance" "myinstance" {
   ami                         = "ami-00ca32bbc84273381"
-  instance_type               = var.env[var.myenv]
+  instance_type               = "t3.micro"
   key_name                    = "key1"
   associate_public_ip_address = true
+  count                       = 1
 
   tags = {
-    Name = "myinstance"
+    Name = "tf-jenkins-ubuntu24"
   }
 }
